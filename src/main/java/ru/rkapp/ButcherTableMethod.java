f2d4f2f42434746326ba35e3656bde28f18be474
arletty <arletty@list.ru>
2025-07-23 15:36:37 +0300

    Правки

--- src/main/java/ru/rkapp/ButcherTableMethod.java
+++ src/main/java/ru/rkapp/ButcherTableMethod.java
@@ -52,63 +52,44 @@ public abstract class ButcherTableMethod extends RungeKuttaMethod {
      */
     @Override
     public boolean step(double t, double[] Y, double h, double[] YN, Object parm) {
-        final int n = Y.length; // Количество уравнений в системе
-        final double[][] k = new double[stages][n]; // Матрица коэффициентов k для каждой стадии
-        final double[] temp = new double[n]; // Временный массив для промежуточных вычислений
+      final int n = Y.length;
+        final double[][] k = new double[stages][n];
+        final double[] temp = new double[n];
         
-         // Вычисление k0 (первая стадия)
+        // Стадия 0 (k0)
         if (!rightCalculator.compute(t, Y, k[0], parm)) {
-            return false; // Ошибка при вычислении правых частей
+            return false;
         }
         
-        // Вычисление k1..k_{s-1}
-        for (int i = 1; i < stages; i++) {
-            // Копирование начальных значений Y во временный массив
+        for (int i = 0; i < stages - 1; i++) {
             System.arraycopy(Y, 0, temp, 0, n);
-            // Расчет индекса для доступа к коэффициентам a
-            int aIndex = i * (i - 1) / 2;
-            // Суммирование влияния предыдущих стадий
-            for (int j = 0; j < i; j++) {
+            
+            int aIndex = (i + 1) * i / 2;
+            
+            for (int j = 0; j <= i; j++) {
                 final double aVal = a[aIndex + j];
-                if (aVal != 0) {
+                if (aVal != 0.0) {  // Оптимизация для нулевых коэффициентов
                     for (int m = 0; m < n; m++) {
-                        // Обновление временного массива
                         temp[m] += h * aVal * k[j][m];
                     }
                 }
             }
-////            // Вычисление времени для текущей стадии
-////            double stageTime = t;
-////            if (i > 1) {
-////                stageTime += c[i-2] * h;
-////            } else if (i == 1 && c.length > 0) {
-////                stageTime += c[0] * h;
-////            }
-////            // Вычисление правых частей для текущей стадии
-////            if (!rightCalculator.compute(stageTime, temp, k[i], parm)) {
-////                return false; // Ошибка при вычислении
-////            }
-        double stageTime = t;
-        if (i - 1 < c.length) {
-            stageTime += c[i - 1] * h;  // Используем c[i-1] вместо c[i-2]
-        }
-        
-        if (!rightCalculator.compute(stageTime, temp, k[i], parm)) {
-            return false;
+            
+            double stageTime = t + c[i] * h;
+            
+            if (!rightCalculator.compute(stageTime, temp, k[i + 1], parm)) {
+                return false;
+            }
         }
 
-        }
-        // Вычисление нового значения Y
         for (int i = 0; i < n; i++) {
-            double sum = 0;
-            // Суммирование вклада всех стадий
+            double sum = 0.0;
             for (int j = 0; j < stages; j++) {
                 sum += b[j] * k[j][i];
             }
-            // Обновление значения переменной
             YN[i] = Y[i] + h * sum;
         }
         
-        return true; // Успешное завершение шага
+        return true;
     }
 }
